ca1927a199b7622caa212f61c52b410dc9a69a0e
shaikhgufran01 <121754290+shaikhgufran01@users.noreply.github.com>
2026-08-26 13:36:00 +0530

    Add files via upload

--- application/static/app.js
+++ application/static/app.js
@@ -40,6 +40,7 @@ createApp({
       // Edit state
       editId: null,
       editName: "",
+      editDueDate: "",
       editCategory: "",
       editPriority: ""
     };
@@ -263,6 +264,7 @@ createApp({
     startEdit(todo) {
       this.editId = todo.id;
       this.editName = todo.name;
+      this.editDueDate = todo.due_date ? todo.due_date.split('T')[0] : '';
       this.editCategory = todo.category || 'General';
       this.editPriority = todo.priority || 'Medium';
     },
@@ -272,6 +274,7 @@ createApp({
       try {
         await axios.put(`/todos/${todo.id}`, {
           name: this.editName,
+          due_date: this.editDueDate,
           category: this.editCategory,
           priority: this.editPriority
         });
